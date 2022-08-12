a5aa7f3371d92fa7ca6e0fa7390df406bdc5b777
Igor <filinigor@yandex.ru>
2022-08-12 17:01:56 +0300

    Updates

---
+++ precache-manifest.6923f94c5866dfc7f695a0f76b611f89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c033bf356b65e4691da9b63275b8c252",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "d4fc26571ac91031438a",
+    "url": "/todolist/static/css/main.6f0a4f0e.chunk.css"
+  },
+  {
+    "revision": "a75f2e4e95a515c433bc",
+    "url": "/todolist/static/js/2.63aa9c4a.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/todolist/static/js/2.63aa9c4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4fc26571ac91031438a",
+    "url": "/todolist/static/js/main.9c730383.chunk.js"
+  },
+  {
+    "revision": "197f51c8578d731c6831",
+    "url": "/todolist/static/js/runtime-main.e7725399.js"
+  }
+]);
